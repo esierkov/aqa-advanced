338555449da612079a7f6135209ff57c74efaf08
Yevhen Sierkov <esierkov@gmail.com>
2026-05-17 13:22:45 +0200

    files: create index.js

---
+++ js-practice/lesson3/node_basic/index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk.green(
+    'They don\'t know ' +
+    chalk.blue.underline.bold('that we know ') +
+    chalk.bgRedBright.bold('they know we know!')
+));
