4a821cb69efefafc5849baa5adff607709820c2c
Yevhen Sierkov <esierkov@gmail.com>
2026-05-19 16:16:35 +0200

    files: create task1.js - task4.js

---
+++ js-practice/lesson4/task1.js
@@ -0,0 +1,11 @@
+let userName = 'Yevhen';
+let age = 26;
+let isHillelStudent = true;
+let emptyValue = null;
+let undefinedVariable;
+
+console.log("String:", userName);
+console.log("Number:", age);
+console.log("Boolean:", isHillelStudent);
+console.log("Null:", emptyValue);
+console.log("Undefined:", undefinedVariable);
---
+++ js-practice/lesson4/task4.js
@@ -0,0 +1,33 @@
+//task 4.1
+console.log('Task 4.1');
+
+let radius = 5;
+let circleArea = Math.PI * radius * radius;
+let roundedCircleArea = Number(circleArea.toFixed(2));
+
+console.log("Circle Radius:", radius);
+console.log("Area of the circle:", roundedCircleArea);
+
+//task 4.2
+console.log('\nTask 4.2');
+
+let rectangleLength = 5.2;
+let rectangleWidth = 3.4;
+let rectangleArea = rectangleLength * rectangleWidth;
+let roundedRectangleArea = Number(rectangleArea.toFixed(2));
+
+console.log("Rectangle Length:", rectangleLength);
+console.log("Rectangle Width:", rectangleWidth);
+console.log('Rectangle Area:', roundedRectangleArea);
+
+//task 4.3
+console.log('\nTask 4.3');
+
+let cylinderRadius = 5;
+let cylinderHeight = 10;
+let cylinderVolume = Math.PI * cylinderRadius * cylinderRadius * cylinderHeight;
+let roundedCylinderVolume = Number(cylinderVolume.toFixed(2));
+
+console.log("Radius:", cylinderRadius);
+console.log("Height:", cylinderHeight);
+console.log("Volume of cylinder:", roundedCylinderVolume);
