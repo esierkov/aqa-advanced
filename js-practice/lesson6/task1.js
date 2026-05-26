18a8bed60107a0efb80760daf312b4497f258651
Yevhen Sierkov <esierkov@gmail.com>
2026-05-26 16:26:05 +0200

    files: create task1.js - task3.js for hw6

---
+++ js-practice/lesson6/task1.js
@@ -0,0 +1,22 @@
+function getRectangleAreaDeclaration(width, height) {
+    return width * height;
+}
+
+const area1 = getRectangleAreaDeclaration(5, 10);
+console.log("Function Declaration:", area1);
+
+
+const getRectangleAreaExpression = function (width, height) {
+    return width * height;
+};
+
+const area2 = getRectangleAreaExpression(5, 10);
+console.log("Function Expression:", area2);
+
+
+const getRectangleAreaArrow = (width, height) => {
+    return width * height;
+};
+
+const area3 = getRectangleAreaArrow(5, 10);
+console.log("Arrow Function:", area3);
---
+++ js-practice/lesson6/task3.js
@@ -0,0 +1,17 @@
+function checkOrder(available, ordered) {
+    if (ordered > available) {
+        return "Your order is too large, we don’t have enough goods.";
+    }
+
+    if (ordered === 0) {
+        return "Your order is empty.";
+    }
+
+    if (available >= ordered) {
+        return "Your order is accepted";
+    }
+}
+
+console.log(checkOrder(7, 10));
+console.log(checkOrder(7, 0));
+console.log(checkOrder(7, 7));
